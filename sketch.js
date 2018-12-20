fe10a7616aed08bdc712a441e8eeb1ce5b22e827
benDebontridder <43341583+benDebontridder@users.noreply.github.com>
2018-12-20 12:06:20 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,64 +1,50 @@
+let kleuren = [{color: "red"}, {color: "blue"}, {color: "grey"}, {color: "yellow"}, {color: "green"}, {color: "purple"}];
 const width = 10;
 const height = 10;
 const spacer = 50;
-let result = [];
+const canvasHeight = 700;
+const canvasWidth = 1000;
+const paddingTop = 50;
+const paddingLeft = 50;
+let grid = [...Array(width)].map(e => Array(height));
+
+let x = 0;
+let y = 0;
+
 
 function Position(x, y){
     this.x = x;
     this.y = y
 }
 
-function Steen(color, position){
+function Stone(color, position){
     this.color = color;
     this.position = position
 }
 
 function setup() {
 
-        for (let i = 0; i !== width ; i++) {
-            result[i] = [];
-            for (let j = 0; j !== height ; j++) {
-                let color = Math.floor(Math.random() * 6);
-                result[i][j] = new Steen(color, new Position(i * spacer, j * spacer))
+    createCanvas(canvasWidth, canvasHeight);
+
+        for (let i = 0; i < width ; i++) {
+            for (let j = 0; j < height ; j++) {
+                let kleur = int(random(6));
+                grid[i][j] = new Stone(kleur, new Position(i * spacer, j * spacer));
             }
         }
-
-    console.log(result);
-
-
+    console.log(grid);
 }
 
 function draw() {
 
-    color = new Array(5);
-    for (let i = 0; i !== innerWidth ; i++) {
-        for (let j = 0; j !== innerHeight ; j++) {
-            if (!(result[i][j].color === 0)) 
-                switch(result[i][j].color){
-                    case 0:
-                        color = "red";
-                        break;
-                    case 1:
-                        color = "green";
-                        break;
-                    case 2:
-                        color = "yellow";
-                        break;
-                    case 3:
-                        color = "blue";
-                        break;
-                    case 4:
-                        color = "white";
-                        break;
-                    case 5:
-                        color = "purple";
-                        break;
-                }
 
+    for (let i = 0; i < grid[0].length ; i++) {
+        for (let j = 0; j < grid.length ; j++) {
+            let stone;
+            let kleur = int(Math.floor(Math.random() * 6));
+            ellipse(i * spacer + paddingTop, j * spacer + paddingLeft, 45, 45);
+            fill(kleuren[kleur].color);
+            noLoop()
         }
     }
-
-
-
-
 }
